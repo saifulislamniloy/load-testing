05935962226aa1584dedc8f1386183a3e54475c2
Md-Monowarul-Amin <aminsaraj2015@gmail.com>
2026-03-04 13:59:59 +0600

    random point outside polygon

--- util/RandomCoordinateGenerator.py
+++ util/RandomCoordinateGenerator.py
@@ -16,3 +16,14 @@ class RandomCoordinateGenerator:
             )
             if polygon.contains(random_point):
                 return random_point.x, random_point.y  # lat, lon
+    
+    @staticmethod
+    def random_point_outside_polygon(inner_polygon, outer_polygon):
+        """
+        Generate a random point inside outer_polygon but outside inner_polygon
+        """
+        while True:
+            lat, lon = RandomCoordinateGenerator.random_point_in_polygon(outer_polygon)
+            point = Point(lon, lat)  # Shapely uses (x, y) = (lon, lat)
+            if not inner_polygon.contains(point):
+                return lat, lon
