785aeccff12d046971a454cd6721a1172af02dd0
Md-Monowarul-Amin <aminsaraj2015@gmail.com>
2026-03-04 12:12:47 +0600

    Dynamic Geofence added

--- zone_location/locustfile.py
+++ zone_location/locustfile.py
@@ -1,6 +1,8 @@
 # locustfile.py
 from locust import HttpUser, task, between, TaskSet
 from load_testing.util.RandomCoordinateGenerator import RandomCoordinateGenerator
+from load_testing.util import constants
+BANGLADESH = constants.BANGLADESH
 
 # --- CONFIG ---
 JWT_TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiI5MGEyZmZjMS1mMTY1LTQ2NDktOGIyZS1jMDJiYmQxNTA3MTIiLCJzdWIiOiI4MDA2OTc4NzY0Mzg5MTMwMjQiLCJpYXQiOjE3NzI1OTc3MDQsImV4cCI6MTc3MzIwMjUwNCwidG9rZW5fdmVyIjoxLCJkZXZpY2VfaWQiOiJzdHJpbmciLCJzdWJfdHlwZSI6IkRSSVZFUiIsInNpZCI6ImI5N2VkYTBjLTI4NjUtNDg2OS1hZjljLTYzMzk0YWQ2MDc5ZiIsInJvbGVzIjpbIlJPTEVfRFJJVkVSIl19.Zie25X8XoM58adEzGfpapae8W5IFxcAi5D5tJKTsjgyeEwmfOfrtj91PDCqMSJhjXrkKc1RCTWs7QNEYncGnDg"  # paste your token here
@@ -24,7 +26,7 @@ class LocationServiceUser(HttpUser):
     def find_zone_by_location(self):
         """Most likely your hottest endpoint"""
 
-        lat, lon = RandomCoordinateGenerator.random_point_in_polygon()
+        lat, lon = RandomCoordinateGenerator.random_point_in_polygon(BANGLADESH)
         print(lat, lon)
         res = self.client.get(
             f"/api/v1/zones/location",
